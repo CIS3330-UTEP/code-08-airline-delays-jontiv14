a032df7207d0dd174faf24f347c34ef59ba259e1
jontiv14 <jontiveros14@miners.utep.edu>
2025-04-06 17:58:00 -0600

    Update code_7.py

--- code_7.py
+++ code_7.py
@@ -1,8 +1,21 @@
 import pandas as pd
-import statsmodels.api as sm
 import matplotlib.pyplot as plt
-#If any of this libraries is missing from your computer. Please install them using pip.
+import seaborn as sns
 
+
+# Descriptive Analytics
 filename = 'Flight_Delays_2018.csv'
+df = pd.read_csv(filename)
+print(df.describe)
 
-#ARR_DELAY is the column name that should be used as dependent variable (Y).
+
+# query = "Identification_Make in ['Toyota', 'Mazda', 'Honda']"
+# df = df.query(query)
+
+# model = sm_api.ols('Dimensions_Length ~ C(Identification_Make)', data = df).fit()
+# anova_table = sm.stats.anova_lm(model,typ=2)
+# print(anova_table)
+
+# tukey_results = pairwise_tukeyhsd(endog=df ["Dimensions_Length"], groups=df["Identification_Make"], alpha =.05)
+# print(tukey_results)
+# #ARR_DELAY is the column name that should be used as dependent variable (Y).
